9c30135eec8883567b405e10846822dbfa605307
tatototino <tatototino@linuxmail.org>
2018-10-01 15:17:13 -0300

    Update main.py

--- simple-api-bottle-etcd/main.py
+++ simple-api-bottle-etcd/main.py
@@ -1,7 +1,5 @@
 
 import etcd
-from concurrent.futures import ThreadPoolExecutor
-import threading
 import string
 import json
 from bottle import route, run, request, abort, post
@@ -9,10 +7,12 @@ from couchbase.cluster import Cluster
 from couchbase.cluster import PasswordAuthenticator
 from time import time
 #client = etcd.Client() # this will create a client against etcd server running on localhost on port 4001
-client = etcd.Client(host='127.0.0.1', port=2379)
+print __name__
+if __name__ == "__main__":
+  client = etcd.Client(host='127.0.0.1', port=2379)
 #db_endpoint = client.read('/etcd')
 #print (db_endpoint.value)
-try:
+  try:
     db_endpoint = client.read('/couchbase', recursive = True)
     i=0
     etcd_endpoint=[]
@@ -34,15 +34,15 @@ try:
           #print endpointetcd[i]
     #      i = i + 1
 
-except etcd.EtcdKeyNotFound:
+  except etcd.EtcdKeyNotFound:
     # do something
     print "Chave deploy/couchbase not found"
 
-db_endpoints=','.join(etcd_endpoint)
-db_endpoint= str('couchbase://' + db_endpoints)
+  db_endpoints=','.join(etcd_endpoint)
+  db_endpoint= str('couchbase://' + db_endpoints)
 
 
-try:
+  try:
     config_variables = client.read('/config/app', recursive = True)
     #print  config_variables.value
     for line in  config_variables.value.split('\n'):
@@ -56,15 +56,9 @@ try:
      if line2[0] == 'bucket_name':
       bucket_name=str(line2[1])
     #data.append([first_part, second_part])
-except etcd.EtcdKeyNotFound:
+  except etcd.EtcdKeyNotFound:
     # do something
     print "Chave /config not found"
-
-print username_db
-print password_db
-print bucket_name
-
-
 
 #print etcd_endpoint[0]
 #db_endpoint = client.read('/etcd-servers',timeout=0, wait = True, recursive = True)
@@ -82,8 +76,8 @@ print bucket_name
 #           etcd_endpoint.append(value)
 #           i = i + 1
 
-@route('/app', method='POST')
-def post_deploy():
+  @route('/app', method='POST')
+  def post_deploy():
       data = request.body.readline()
       if not data:
         abort(400, 'No data received')
@@ -104,4 +98,4 @@ def post_deploy():
       datadb = json.dumps(data2,sort_keys=True)
       cb.upsert(id, datadb)
       return datadb
-run(host='0.0.0.0', port=8080)
+  run(host='0.0.0.0', port=8080)
